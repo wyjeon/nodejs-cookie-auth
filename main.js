b542d015836de6902b3112e364ce99b58b51b283
wyjeon <wyjeon.info@gmail.com>
2021-01-29 15:10:33 +0900

    9.6.인증구현 - 로그아웃

--- main.js
+++ main.js
@@ -214,6 +214,23 @@ var app = http.createServer(function (request, response) {
         response.end("Who?");
       }
     });
+  } else if (pathname === "/logout_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body = body + data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      response.writeHead(302, {
+        "Set-Cookie": [
+          `email=; Max-Age=0`,
+          `password=; Max-Age=0`,
+          `nickname=; Max-Age=0`,
+        ],
+        Location: `/`,
+      });
+      response.end();
+    });
   } else {
     response.writeHead(404);
     response.end("Not found");
