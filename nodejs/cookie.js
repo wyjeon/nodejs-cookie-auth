59182d4fca8b8a30be5082b212d3db051795c5d8
wyjeon <wyjeon.info@gmail.com>
2021-01-29 14:31:29 +0900

    8. path & domain

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -13,6 +13,8 @@ http
         `Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`,
         "Secure=Secure; Secure",
         "HttpOnly=HttpOnly; HttpOnly",
+        "Path=Path; Path=/cookie",
+        "Doamin=Domain; Domain=test.o2.org",
       ],
     });
     response.end("Cookie!");
