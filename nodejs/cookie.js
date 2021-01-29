d8d9c8cd51f6c62d1ce5938da3ae65f776aed5d5
wyjeon <wyjeon.info@gmail.com>
2021-01-29 14:18:31 +0900

    6.Session vs Permanent

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -7,7 +7,11 @@ http
       cookies = cookie.parse(request.headers.cookie);
     }
     response.writeHead(200, {
-      "Set-Cookie": ["yummy_cookie=choco", "tasty_cookie=strawberry"],
+      "Set-Cookie": [
+        "yummy_cookie=choco",
+        "tasty_cookie=strawberry",
+        `Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`,
+      ],
     });
     response.end("Cookie!");
   })
