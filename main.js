d3cb3187e0cc86a02c1771f10b94c271eab9d9f0
wyjeon <wyjeon.info@gmail.com>
2021-01-29 14:59:56 +0900

    9.4. 인증구현 - 로그인 상태 체크

--- main.js
+++ main.js
@@ -5,11 +5,29 @@ var qs = require("querystring");
 var template = require("./lib/template.js");
 var path = require("path");
 var sanitizeHtml = require("sanitize-html");
+var cookie = require("cookie");
+
+function authIsOwner(request, response) {
+  var isOwner = false;
+  var cookies = {};
+  if (request.headers.cookie) {
+    cookies = cookie.parse(request.headers.cookie);
+  }
+  if (
+    cookies.email === "egoing777@gmail.com" &&
+    cookies.password === "111111"
+  ) {
+    isOwner = true;
+  }
+  return isOwner;
+}
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
+  var isOwner = authIsOwner(request, response);
+  console.log(isOwner);
   if (pathname === "/") {
     if (queryData.id === undefined) {
       fs.readdir("./data", function (error, filelist) {
