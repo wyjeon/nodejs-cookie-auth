a5a38a33fd7130ddb5fa6e6cce1e647e8e34eaa7
wyjeon <wyjeon.info@gmail.com>
2021-01-29 14:24:22 +0900

    7.Secure & HttpOnly

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -11,6 +11,8 @@ http
         "yummy_cookie=choco",
         "tasty_cookie=strawberry",
         `Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`,
+        "Secure=Secure; Secure",
+        "HttpOnly=HttpOnly; HttpOnly",
       ],
     });
     response.end("Cookie!");
